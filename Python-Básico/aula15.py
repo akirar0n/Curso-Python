e6da3afd24918592b90b9d94a88c918b0e02fbbf
RoneyVila <roneyvns@gmail.com>
2025-10-01 11:38:36 -0300

    new commit

---
+++ Python-Básico/aula15.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite outro número: ')
+
+print(f'A soma dos números é {numero1 + numero2}')  # concatenação de string
